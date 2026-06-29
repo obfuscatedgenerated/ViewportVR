bf5a2a57edcb860c3f1d7ef76f72d4bf5c153fef
Ollie <75035364+obfuscatedgenerated@users.noreply.github.com>
2026-06-30 00:14:00 +0100

    mouth test buttons

--- packages/vr-engine/src/player/AvatarExpression.tsx
+++ packages/vr-engine/src/player/AvatarExpression.tsx
@@ -70,3 +70,4 @@ export const AvatarExpression = () => {
         </LayerGroup>
     );
 }
+// TODO: 2-frame animation style
--- packages/vr-engine/src/player/Player.tsx
+++ packages/vr-engine/src/player/Player.tsx
@@ -2,10 +2,47 @@ import { useXRControllerLocomotion, XROrigin } from "@react-three/xr";
 import { useImperativeHandle, useRef } from "react";
 import { Group } from "three";
 
+import { Text } from "@react-three/drei";
+
+import {
+    ExpressionMouth,
+    PlayerExpressionProvider,
+    usePlayerExpression
+} from "../contexts/PlayerExpressionContext";
 import { Avatar } from "./Avatar";
 import { WristWatch } from "./WristWatch";
-import { PlayerExpressionProvider } from "../contexts/PlayerExpressionContext";
 
+
+const MouthTest = ({mouth_name, position}: {mouth_name: ExpressionMouth, position: [number, number, number]}) => {
+    const { set_mouth } = usePlayerExpression();
+
+    return (
+        <group name="MouthTest" position={position} onClick={() => set_mouth(mouth_name)}>
+            <mesh
+                name="MouthTestPlane"
+            >
+                <planeGeometry args={[0.3, 0.3]} />
+                <meshBasicMaterial
+                    color="white"
+                    transparent
+                    opacity={0.5}
+                    side={2}
+                />
+            </mesh>
+
+            <Text
+                name="MouthTestText"
+                position={[0, 0, 0.01]}
+                fontSize={0.05}
+                color="black"
+                anchorX="center"
+                anchorY="middle"
+            >
+                {mouth_name}
+            </Text>
+        </group>
+    );
+}
 
 export const Player = ({ref = null}: {ref?: React.Ref<Group>}) => {
     const origin_ref = useRef<Group>(null);
@@ -18,8 +55,21 @@ export const Player = ({ref = null}: {ref?: React.Ref<Group>}) => {
             <PlayerExpressionProvider>
                 <XROrigin ref={origin_ref}>
                     <WristWatch />
+
+                    <Text
+                        position={[0, 2, -1]}
+                        fontSize={0.1}
+                        color="white"
+                        anchorX="center"
+                        anchorY="middle"
+                    >
+                        Mouth Expression Test
+                    </Text>
+                    <MouthTest mouth_name="default" position={[-0.5, 1.5, -1]} />
+                    <MouthTest mouth_name="big_smile" position={[0, 1.5, -1]} />
+                    <MouthTest mouth_name="wobbly_frown" position={[0.5, 1.5, -1]} />
                 </XROrigin>
-    
+
                 <Avatar />
             </PlayerExpressionProvider>
         </group>
