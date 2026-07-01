fd09b329034a4cac1a45d4e007b9ff82dd5e192e
Ollie <75035364+obfuscatedgenerated@users.noreply.github.com>
2026-07-01 21:54:03 +0100

    common methods on builders (from_data and clone) using protected accumulator

--- packages/web-sdk/src/builders.ts
+++ packages/web-sdk/src/builders.ts
@@ -43,292 +43,365 @@ import {
 
 import { send_via_rtc } from "./messenger";
 
-export class PhysicsSystemBuilder {
-    #physics: PhysicsSystemInput = {};
+class BaseBuilder<InternalType> {
+    protected _internal: InternalType;
+
+    constructor(initial: InternalType) {
+        this._internal = initial;
+    }
+
+    static from_data<B extends BaseBuilder<any>, D>(this: new (data: D) => B, data: D): B {
+        return new this(JSON.parse(JSON.stringify(data)));
+    }
+
+    clone(): this {
+        return (this.constructor as any).from_data(this._internal);
+    }
+}
+
+export class PhysicsSystemBuilder extends BaseBuilder<PhysicsSystemInput> {
+    constructor() {
+        super({});
+    }
 
     set_rigid_body(body: RigidBody) {
-        this.#physics.rigid_body = body;
+        this._internal.rigid_body = body;
         return this;
     }
 
     build(): PhysicsSystem {
-        return PhysicsSystemSchema.parse(this.#physics);
+        return PhysicsSystemSchema.parse(this._internal);
     }
 }
 
-export class RigidBodyBuilder {
-    #rigid_body = {} as RigidBodyInput;
-
-    set_type(type: RigidBodyType) {
-        this.#rigid_body.type = type;
-        return this;
+class RigidBodyBuilder extends BaseBuilder<RigidBodyInput> {
+    constructor(type: RigidBodyType) {
+        super({ type } as RigidBodyInput);
     }
 
     set_collider(collider: Collider) {
-        this.#rigid_body.collider = collider;
+        this._internal.collider = collider;
         return this;
     }
 
-    set_mass(mass: number) {
-        if (this.#rigid_body.type !== "dynamic") {
+    protected _try_set_mass(mass: number) {
+        if (this._internal.type !== "dynamic") {
             throw new Error("Mass can only be set for dynamic rigid bodies.");
         }
 
-        this.#rigid_body.mass = mass;
+        this._internal.mass = mass;
         return this;
     }
 
-    set_velocity(velocity: [number, number, number]) {
-        if (this.#rigid_body.type !== "dynamic" && this.#rigid_body.type !== "kinematic-vel") {
-            throw new Error("Velocity can only be set for dynamic or kinematic-vel rigid bodies.");
+    protected _try_set_velocity(velocity: [number, number, number]) {
+        if (
+            this._internal.type !== "dynamic" &&
+            this._internal.type !== "kinematic-vel"
+        ) {
+            throw new Error(
+                "Velocity can only be set for dynamic or kinematic-vel rigid bodies."
+            );
         }
 
-        this.#rigid_body.velocity = velocity;
+        this._internal.velocity = velocity;
         return this;
     }
 
     build(): RigidBody {
-        return RigidBodySchema.parse(this.#rigid_body);
+        return RigidBodySchema.parse(this._internal);
     }
 }
 
+export class DynamicRigidBodyBuilder extends RigidBodyBuilder {
+    constructor() {
+        super("dynamic");
+    }
 
-export class ColliderBuilder {
-    #collider: Collider = {type: "copy-visual-mesh"};
+    set_mass(mass: number) {
+        return this._try_set_mass(mass);
+    }
+
+    set_velocity(velocity: [number, number, number]) {
+        return this._try_set_velocity(velocity);
+    }
+}
+
+export class KinematicPosRigidBodyBuilder extends RigidBodyBuilder {
+    constructor() {
+        super("kinematic-pos");
+    }
+}
+
+export class KinematicVelRigidBodyBuilder extends RigidBodyBuilder {
+    constructor() {
+        super("kinematic-vel");
+    }
+
+    set_velocity(velocity: [number, number, number]) {
+        return this._try_set_velocity(velocity);
+    }
+}
+
+export class FixedRigidBodyBuilder extends RigidBodyBuilder {
+    constructor() {
+        super("fixed");
+    }
+}
+
+export class ColliderBuilder extends BaseBuilder<Collider> {
+    constructor() {
+        super({ type: "copy-visual-mesh" });
+    }
 
     box(size: [number, number, number]) {
-        this.#collider = { type: "box", size };
+        this._internal = { type: "box", size };
         return this;
     }
 
     sphere(radius: number) {
-        this.#collider = { type: "sphere", radius };
+        this._internal = { type: "sphere", radius };
         return this;
     }
 
     capsule(radius: number, height: number) {
-        this.#collider = { type: "capsule", radius, height };
+        this._internal = { type: "capsule", radius, height };
         return this;
     }
 
     custom_mesh(mesh_url: string) {
-        this.#collider = { type: "custom-mesh", mesh: mesh_url };
+        this._internal = { type: "custom-mesh", mesh: mesh_url };
         return this;
     }
 
     copy_visual_mesh() {
-        this.#collider = { type: "copy-visual-mesh" };
+        this._internal = { type: "copy-visual-mesh" };
         return this;
     }
 
     build(): Collider {
-        return ColliderSchema.parse(this.#collider);
+        return ColliderSchema.parse(this._internal);
     }
 }
 
-export class GrabbableInteractionBuilder {
-    #interaction: GrabbableInteractionInput = {
-        type: "grabbable"
-    };
+export class GrabbableInteractionBuilder extends BaseBuilder<GrabbableInteractionInput> {
+    constructor() {
+        super({
+            type: "grabbable"
+        });
+    }
 
     set_grab_distance(distance: number) {
-        this.#interaction.grab_distance = distance;
+        this._internal.grab_distance = distance;
         return this;
     }
 
     set_grab_offset(offset: GrabOffsetInput) {
-        this.#interaction.grab_offset = offset;
+        this._internal.grab_offset = offset;
         return this;
     }
 
     // these ones default to true, so having a default boolean here doesnt make sense, must explicitly set to false to disable
 
     set_sticky(sticky: boolean) {
-        this.#interaction.sticky = sticky;
+        this._internal.sticky = sticky;
         return this;
     }
 
     set_snaps_to_hand(snaps: boolean) {
-        this.#interaction.snaps_to_hand = snaps;
+        this._internal.snaps_to_hand = snaps;
         return this;
     }
 
     // these below are default false, so specifying .reports_grabs() should make it true by default
 
     reports_grabs(reports = true) {
-        this.#interaction.report_grabs = reports;
+        this._internal.report_grabs = reports;
         return this;
     }
 
     reports_releases(reports = true) {
-        this.#interaction.report_releases = reports;
+        this._internal.report_releases = reports;
         return this;
     }
 
     reports_proximity(reports = true) {
-        this.#interaction.report_proximity = reports;
+        this._internal.report_proximity = reports;
         return this;
     }
 
     build(): GrabbableInteraction {
-        return GrabbableInteractionSchema.parse(this.#interaction);
+        return GrabbableInteractionSchema.parse(this._internal);
     }
 }
 
-export class ControllerButtonInteractionBuilder {
-    #interaction = { type: "controller-button" } as ControllerButtonInteractionInput;
+export class ControllerButtonInteractionBuilder extends BaseBuilder<ControllerButtonInteractionInput> {
+    constructor() {
+        super({
+            type: "controller-button"
+        } as ControllerButtonInteractionInput);
+    }
 
     set_button(button: string) {
-        this.#interaction.button = button;
+        this._internal.button = button;
         return this;
     }
 
     set_when_listen(when: ControllerButtonWhenListen) {
-        this.#interaction.when_listen = when;
+        this._internal.when_listen = when;
         return this;
     }
 
     set_reports_press(reports: boolean) {
-        this.#interaction.report_press = reports;
+        this._internal.report_press = reports;
         return this;
     }
 
     set_reports_release(reports: boolean) {
-        this.#interaction.report_release = reports;
+        this._internal.report_release = reports;
         return this;
     }
 
     build(): ControllerButtonInteraction {
-        return ControllerButtonInteractionSchema.parse(this.#interaction);
+        return ControllerButtonInteractionSchema.parse(this._internal);
     }
 }
 
-export class TriggerVolumeInteractionBuilder {
-    #interaction = { type: "trigger-volume" } as TriggerVolumeInteractionInput;
+export class TriggerVolumeInteractionBuilder extends BaseBuilder<TriggerVolumeInteractionInput> {
+    constructor() {
+        super({ type: "trigger-volume" } as TriggerVolumeInteractionInput);
+    }
 
     set_collider(collider: Collider) {
-        this.#interaction.collider = collider;
+        this._internal.collider = collider;
         return this;
     }
 
     set_reports_enter(reports: boolean) {
-        this.#interaction.report_enter = reports;
+        this._internal.report_enter = reports;
         return this;
     }
 
     set_reports_exit(reports: boolean) {
-        this.#interaction.report_exit = reports;
+        this._internal.report_exit = reports;
         return this;
     }
 
     ignore_hands(ignore = true) {
-        this.#interaction.ignore_hands = ignore;
+        this._internal.ignore_hands = ignore;
         return this;
     }
 
     ignore_torso(ignore = true) {
-        this.#interaction.ignore_torso = ignore;
+        this._internal.ignore_torso = ignore;
         return this;
     }
 
     ignore_head(ignore = true) {
-        this.#interaction.ignore_head = ignore;
+        this._internal.ignore_head = ignore;
         return this;
     }
 
     build(): TriggerVolumeInteraction {
-        return TriggerVolumeInteractionSchema.parse(this.#interaction);
+        return TriggerVolumeInteractionSchema.parse(this._internal);
     }
 }
 
-
-export class CustomObjectBuilder {
-    #object = { type: "custom" } as CustomObjectInput;
+export class CustomObjectBuilder extends BaseBuilder<CustomObjectInput> {
+    constructor() {
+        super({ type: "custom" } as CustomObjectInput);
+    }
 
     set_mesh(glb_url: string) {
-        this.#object.mesh = glb_url;
+        this._internal.mesh = glb_url;
         return this;
     }
 
     set_physics(physics: PhysicsSystem) {
-        this.#object.physics = physics;
+        this._internal.physics = physics;
         return this;
     }
 
     add_interaction(interaction: Interaction) {
-        if (!this.#object.interactions) {
-            this.#object.interactions = [];
+        if (!this._internal.interactions) {
+            this._internal.interactions = [];
         }
-        this.#object.interactions.push(interaction);
+        this._internal.interactions.push(interaction);
         return this;
     }
 
     add_interactions(interactions: Interaction[]) {
-        if (!this.#object.interactions) {
-            this.#object.interactions = [];
+        if (!this._internal.interactions) {
+            this._internal.interactions = [];
         }
-        this.#object.interactions.push(...interactions);
+        this._internal.interactions.push(...interactions);
         return this;
     }
 
     set_interactions(interactions: Interaction[]) {
-        this.#object.interactions = interactions;
+        this._internal.interactions = interactions;
         return this;
     }
 
     build(): CustomObject {
-        return CustomObjectSchema.parse(this.#object);
+        return CustomObjectSchema.parse(this._internal);
     }
 }
 
-export class ButtonPrefabBuilder {
-    #prefab = { name: "button" } as ButtonPrefabInput;
+export class ButtonPrefabBuilder extends BaseBuilder<ButtonPrefabInput> {
+    constructor() {
+        super({ name: "button" } as ButtonPrefabInput);
+    }
 
     set_label(label: string) {
-        this.#prefab.label = label;
+        this._internal.label = label;
         return this;
     }
 
     set_color(color: HexNumericalColor) {
-        this.#prefab.color = HexNumericalColorSchema.parse(color);
+        this._internal.color = HexNumericalColorSchema.parse(color);
         return this;
     }
 
     build(): ButtonPrefab {
-        return ButtonPrefabSchema.parse(this.#prefab);
+        return ButtonPrefabSchema.parse(this._internal);
     }
 }
 
-class AxesBasedMonitorBuilder {
-    #monitor = {} as AxesBasedMonitorInput;
+class AxesBasedMonitorBuilder extends BaseBuilder<AxesBasedMonitorInput> {
+    constructor(
+        type: "position" | "rotation" | "linear-velocity" | "angular-velocity",
+        name: string
+    ) {
+        super({ type, name });
+    }
 
-    constructor(type: "position" | "rotation" | "linear-velocity" | "angular-velocity", name: string) {
-        this.#monitor.name = name;
-        this.#monitor.type = type;
+    rename(name: string) {
+        this._internal.name = name;
+        return this;
     }
 
     when(cond: "any" | "all" | "xor") {
-        this.#monitor.when = cond;
+        this._internal.when = cond;
         return this;
     }
 
     x(range: AxisRange) {
-        this.#monitor.x = range;
+        this._internal.x = range;
         return this;
     }
 
     y(range: AxisRange) {
-        this.#monitor.y = range;
+        this._internal.y = range;
         return this;
     }
 
     z(range: AxisRange) {
-        this.#monitor.z = range;
+        this._internal.z = range;
         return this;
     }
 
     build(): Monitor {
-        return MonitorSchema.parse(this.#monitor);
+        return MonitorSchema.parse(this._internal);
     }
 }
 
@@ -356,87 +429,89 @@ export class AngularVelocityMonitorBuilder extends AxesBasedMonitorBuilder {
     }
 }
 
-export class EngineObjectDispatchBuilder {
-    #dispatch = {} as EngineObjectDispatchInput;
+export class EngineObjectDispatchBuilder extends BaseBuilder<EngineObjectDispatchInput> {
+    constructor() {
+        super({} as EngineObjectDispatchInput);
+    }
 
     set_object(object: EngineObject) {
-        this.#dispatch.object = object;
+        this._internal.object = object;
         return this;
     }
 
     set_position(x: number, y: number, z: number) {
-        if (!this.#dispatch.transform) {
-            this.#dispatch.transform = {};
+        if (!this._internal.transform) {
+            this._internal.transform = {};
         }
-        this.#dispatch.transform.position = [x, y, z];
+        this._internal.transform.position = [x, y, z];
         return this;
     }
 
     set_euler_rotation(x: number, y: number, z: number) {
-        if (!this.#dispatch.transform) {
-            this.#dispatch.transform = {};
+        if (!this._internal.transform) {
+            this._internal.transform = {};
         }
-        this.#dispatch.transform.rotation = [x, y, z];
+        this._internal.transform.rotation = [x, y, z];
         return this;
     }
 
     set_quaternion_rotation(x: number, y: number, z: number, w: number) {
-        if (!this.#dispatch.transform) {
-            this.#dispatch.transform = {};
+        if (!this._internal.transform) {
+            this._internal.transform = {};
         }
-        this.#dispatch.transform.rotation = [x, y, z, w];
+        this._internal.transform.rotation = [x, y, z, w];
         return this;
     }
 
     set_scale(x: number, y: number, z: number) {
-        if (!this.#dispatch.transform) {
-            this.#dispatch.transform = {};
+        if (!this._internal.transform) {
+            this._internal.transform = {};
         }
-        this.#dispatch.transform.scale = [x, y, z];
+        this._internal.transform.scale = [x, y, z];
         return this;
     }
 
     set_transform(transform: TransformInput) {
-        this.#dispatch.transform = transform;
+        this._internal.transform = transform;
         return this;
     }
 
     set_user_data_value(key: string, value: any) {
-        if (!this.#dispatch.user_data) {
-            this.#dispatch.user_data = {};
+        if (!this._internal.user_data) {
+            this._internal.user_data = {};
         }
-        this.#dispatch.user_data[key] = value;
+        this._internal.user_data[key] = value;
         return this;
     }
 
     set_user_data(user_data: Record<string, any>) {
-        this.#dispatch.user_data = user_data;
+        this._internal.user_data = user_data;
         return this;
     }
 
     add_monitor(monitor: Monitor) {
-        if (!this.#dispatch.monitors) {
-            this.#dispatch.monitors = [];
+        if (!this._internal.monitors) {
+            this._internal.monitors = [];
         }
-        this.#dispatch.monitors.push(monitor);
+        this._internal.monitors.push(monitor);
         return this;
     }
 
     add_monitors(monitors: Monitor[]) {
-        if (!this.#dispatch.monitors) {
-            this.#dispatch.monitors = [];
+        if (!this._internal.monitors) {
+            this._internal.monitors = [];
         }
-        this.#dispatch.monitors.push(...monitors);
+        this._internal.monitors.push(...monitors);
         return this;
     }
 
     set_monitors(monitors: Monitor[]) {
-        this.#dispatch.monitors = monitors;
+        this._internal.monitors = monitors;
         return this;
     }
 
     build(): EngineObjectDispatch {
-        return EngineObjectDispatchSchema.parse(this.#dispatch);
+        return EngineObjectDispatchSchema.parse(this._internal);
     }
 
     async create(): Promise<CreatedEngineObject> {
@@ -456,8 +531,7 @@ export class EngineObjectDispatchBuilder {
 const sword = new CustomObjectBuilder()
     .set_mesh("https://example.com/sword.glb")
     .set_physics(new PhysicsSystemBuilder()
-        .set_rigid_body(new RigidBodyBuilder()
-            .set_type("dynamic")
+        .set_rigid_body(new DynamicRigidBodyBuilder()
             .set_collider(new ColliderBuilder().box([0.1, 1, 0.1]).build())
             .set_mass(2)
             .build()
